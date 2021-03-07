fc3866e3aed8ac4ab927658cff9b29f3111c70e9
youmingsama <FLDYSYMhahaha229@outlook.com>
2021-03-07 11:45:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pixvi/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.pixvi
+
+import android.app.DownloadManager
+import android.content.Context
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.view.ContextMenu
+import android.view.ContextMenu.ContextMenuInfo
+import android.view.MenuItem
+import android.view.View
+import android.webkit.URLUtil
+import android.webkit.WebView.HitTestResult
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val decorView=window.decorView
+        decorView.systemUiVisibility=View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor=Color.TRANSPARENT
+        setContentView(R.layout.activity_main)
+        web.settings.javaScriptEnabled=true
+        web.webViewClient= WebViewClient()
+        web.settings.domStorageEnabled=true
+        registerForContextMenu(web);
+        web.loadUrl("https://lab.getloli.com/pixiv-viewer")
+
+    }
+    override fun onCreateContextMenu(contextMenu: ContextMenu, view: View?, contextMenuInfo: ContextMenuInfo?) {
+        super.onCreateContextMenu(contextMenu, view, contextMenuInfo)
+        val webViewHitTestResult: HitTestResult = web.getHitTestResult()
+        if (webViewHitTestResult.type == HitTestResult.IMAGE_TYPE ||
+                webViewHitTestResult.type == HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
+            contextMenu.setHeaderTitle("网页中下载图片")
+            contextMenu.add(0, 1, 0, "点击保存")
+                    .setOnMenuItemClickListener(object : MenuItem.OnMenuItemClickListener {
+                        override fun onMenuItemClick(menuItem: MenuItem?): Boolean {
+                            val DownloadImageURL = webViewHitTestResult.extra
+                            if (URLUtil.isValidUrl(DownloadImageURL)) {
+                                val request = DownloadManager.Request(Uri.parse(DownloadImageURL))
+                                request.allowScanningByMediaScanner()
+                                //设置图片的保存路径
+                                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).mkdir() ;
+                                request.setDestinationInExternalPublicDir( Environment.DIRECTORY_DOWNLOADS, UUID.randomUUID().toString()+".png")
+                                val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                                downloadManager.enqueue(request)
+                                Toast.makeText(this@MainActivity, "下载成功", Toast.LENGTH_LONG).show()
+                            } else {
+                                Toast.makeText(this@MainActivity, "下载失败", Toast.LENGTH_LONG).show()
+                            }
+                            return false
+                        }
+                    })
+        }
+    }
+
+
+}
